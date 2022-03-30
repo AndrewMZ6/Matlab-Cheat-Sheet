f15e20c043cfd92c11ac11974b5db84c7e822bd8
AndrewMZ <andreika3981@gmail.com>
2022-03-30 11:23:29 +0700

    asdadq wd q wd

--- BPSK_procedure.m
+++ BPSK_procedure.m
@@ -1,4 +1,4 @@
-clear all;
+clearvars;
 close all;
 
 % Выбираем размер BPSK последовательности
---
+++ newMATsctript.m
@@ -0,0 +1,5 @@
+% this is a sample script for adding a new script to git
+
+disp('hello');
+
+% this is some changes
